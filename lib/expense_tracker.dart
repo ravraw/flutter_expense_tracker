ba3e438960975821d2872d423493b3cfff5f763b
“ravinrawat” <ravinrawat.dev@gmail.com>
2024-01-20 13:34:31 -0700

    flutter_expense_tracker: Added ExpenseTracker Weidget

---
+++ lib/expense_tracker.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExpenseTracker extends StatefulWidget {
+  const ExpenseTracker({Key? key}) : super(key: key);
+
+  @override
+  State<ExpenseTracker> createState() => _ExpenseTrackerState();
+}
+
+class _ExpenseTrackerState extends State<ExpenseTracker> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World");
+  }
+}
