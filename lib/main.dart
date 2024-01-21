d3279fcefd5b299e00969e70427fec01a0f283b8
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-01-21 14:47:25 -0700

    Updated main theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/expense_tracker.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.blue,
+  brightness: Brightness.light,
+);
+
 main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ExpenseTracker(),
+    title: "Expense Tracker",
+    theme: ThemeData().copyWith(
+      useMaterial3: true,
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.primary,
+        foregroundColor: kColorScheme.onPrimary,
+      ),
+      cardTheme: CardTheme(
+        color: kColorScheme.surface,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    ),
+    home: const ExpenseTracker(),
   ));
 }
