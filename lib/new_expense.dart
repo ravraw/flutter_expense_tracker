afd67862a7c9bbb1bf5d3d27fa486b2454ff31b7
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-01-21 11:19:02 -0700

    Added expense Category to new expense dropdown

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' hide Category;
+import 'package:flutter_expense_tracker/models/expense.dart' as expense_model;
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -68,6 +71,20 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              DropdownButton(
+                items: expense_model.Category.values
+                    .map((category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(describeEnum(category)
+                                  .characters
+                                  .first
+                                  .toUpperCase() +
+                              describeEnum(category).substring(1)),
+                        ))
+                    .toList(),
+                onChanged: (value) {},
+                hint: const Text("Category"),
+              ),
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pop();
